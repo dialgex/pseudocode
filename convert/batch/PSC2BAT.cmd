048c5cfb30078e8760e7c79fbace44b0331ed519
dialgex <dialgex@gmail.com>
2016-08-10 06:16:30 -0400

    Examples

--- convert/batch/PSC2BAT.cmd
+++ convert/batch/PSC2BAT.cmd
@@ -1,8 +1,10 @@
 @echo off
+Setlocal EnableDelayedExpansion
 cls
 
 :Set_vars
-set PSC_dir=".\Put .psc files here"
+set PSC_dir="Put_files_here"
+set cmd_input=%*
 :Set_vars_end
 
 :title
@@ -35,6 +37,17 @@ if not exist %PSC_dir% (
 )
 :dir_check_end
 
+
+:CMD_Input_Check
+if not "%cmd_input%"=="" (
+	echo 	Input found
+	echo 	%cmd_input%
+	call :Dictionary %cmd_input%
+	pause
+)
+:CMD_Input_Check_End
+
+
 :Main_menu
 echo.
 echo 	[1] 	- Convert all PSC files to BAT
@@ -44,17 +57,43 @@ echo.
 
 choice /n /c 13 /m "[----->  "
 
-if %errorlevel%==2 goto:Information
-if %errorlevel%==1 goto:Dictionary 
+if %errorlevel% == 2 goto:Information
+if %errorlevel% == 1 goto:PSC_check
+goto:eof
 :Main_menu_end
 
-:PSC_file_check
 
-:PSC_file_check_end
+:PSC_check
+echo PSC_check
+SETLOCAL
+for %%a in (%PSC_dir%\*) do (
+	set psc_in=%%a
+	if "!psc_in!"=="" (
+		echo Directory empty
+		pause
+		goto:Title
+	) else call :Dictionary !psc_in!
+)
+ENDLOCAL
+goto:eof
+:PSC_check_end
 
 
 :Dictionary
-
+for /f "tokens=*" %%b in (%1) do (
+	set check_line=%%b
+	for /f "tokens=1-3" %%c in ("!check_line!") do (
+		set check_string=%%c
+		if "!check_string:~0,1!"=="$" (
+			echo Found Variable %%c
+			if "%%d"=="=" (
+				set !check_string:$=! = %%e
+				echo !check_string:$=!
+			)
+		)
+	)
+)
+goto:eof
 :Dictionary_end
 pause
 goto:eof
